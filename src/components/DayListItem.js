74cb52ee91e332e48373cddfe30a532a004ac70d
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro.local>
2022-10-05 17:20:20 -0400

    Implement daylist items and daylist; Added daylist to application.js

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "components/DayListItem.scss"
+
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  
+  const formatSpots = () => {
+    if (props.spots === 0){
+      return "no spots remaining"
+    }
+
+    if (props.spots == 1){
+      return "1 spot remaining"
+    }
+
+    else {
+      return `${props.spots} spots remaining`
+    }
+
+  }
+  
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li >
+    );
+  }
