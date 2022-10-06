e92ac33968f77238e86e6918a66acdbcb9025a1a
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro.local>
2022-10-06 17:36:56 -0400

    Made empty and header components; wrote stories for both components

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js"
 
 storiesOf("Button", module)
   .addParameters({
@@ -142,4 +144,6 @@ storiesOf("Button", module)
             backgrounds: [{ name: "white", value: "#fff", default: true}]
           })
           .add("Appointment", () => <Appointment />)
-          .add("Appointment with Time", () => <Appointment  time={"12pm"}/>);
+          .add("Appointment with Time", () => <Appointment  time="12pm"/>);
+          .add("Header", () => <Header time="12pm"/>);
+          .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
