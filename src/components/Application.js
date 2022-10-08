ec3f262ae2c465798c4f16c811aac15cd86852b0
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro.local>
2022-10-08 14:22:29 -0400

    Added axios and made a fetch request to /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -6,23 +7,7 @@ import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = {
   "1": {
@@ -65,7 +50,15 @@ const appointments = {
 
 export default function Application(props) {
 
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      setDays(response.data);
+     
+    })
+  },[])
 
   const appointmentsArr = Object.values(appointments).map((appointment) => {
     return (
